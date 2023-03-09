46371f8b7b3552e643ffc5587501bbb845514ad2
ik250692 <110094738+ik250692@users.noreply.github.com>
2023-03-09 23:19:29 +0600

    Update kursiv.py

--- kursiv.py
+++ kursiv.py
@@ -11,8 +11,8 @@ class Tv7Spider(CrawlSpider):
     name = 'kursiv'
     allowed_domains = ['kz.kursiv.media']
     a = date.today() - timedelta(days=1)
-    b = a.strftime('%Y-%m-%d')
-    start_urls = ['https://kz.kursiv.media/' + b + '/']
+    b = a.strftime('%Y/%m/%d')
+    start_urls = ['https://kz.kursiv.media/archive/'+b+'/']
     handle_httpstatus_list = [401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 500, 501, 502, 503, 504, 505]
     rules = (
         Rule(LinkExtractor(allow=('//'), deny=('docs/', 'cms/', 'app/')),
@@ -20,12 +20,15 @@ class Tv7Spider(CrawlSpider):
     )
     def parse(self, response):
         if response.status == 200:
+            time.sleep(3)
             Item = TutorialItem()
             Item['date_parse'] = datetime.now()
             Item['link'] = response.url
             Item['status'] = response.status
             Item['title'] = response.xpath('//header[contains(@class, "single-header")]/h1/text()').get()
             date_news = dateparser.parse(response.xpath('//span[contains(@class, "single-publishing-time__original")]/time/text()').get())
+            print(f'date_news = {date_news}')
+            print(self.a)
             if date_news.date() == self.a:
                 Item['date_news'] = date_news
                 Item['description'] = response.xpath('//main[contains(@class, "single-body")]/p/text()').getall()
