fde294da3a3f234c12b7adcfb4aefee9774db96f
ik250692 <110094738+ik250692@users.noreply.github.com>
2023-03-09 23:18:40 +0600

    Update kt_kz.py

--- kt_kz.py
+++ kt_kz.py
@@ -16,15 +16,19 @@ class Tv7Spider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(allow=('//'), deny=('/video/', '/gallery/', '/pisma_v_redakciu/')),
              callback="parse", follow=True),)
-    def parse(self, response):
+ def parse(self, response):
         if response.status == 200:
-            time.sleep(20)
+            time.sleep(3)
             Item = TutorialItem()
             Item['date_parse'] = datetime.now()
             Item['link'] = response.url
             Item['status'] = response.status
             Item['title'] = response.xpath('//header[contains(@class, "page-heading")]/h1/text()').get()
-            date_news = dateparser.parse(response.xpath('//div[contains(@class, "page-meta")]/span/text()').get())
+            date_str = response.xpath('//div[contains(@class, "page-meta")]/span/text()').get()
+            date_news = datetime.strptime(date_str, '%d.%m.%Y, %H:%M')
+            print(f'date_news = {date_news}')
+            self.a = date.today() - timedelta(days=1)
+            print(f'self.a = {self.a}')
             if date_news.date() == self.a:
                 Item['date_news'] = date_news
                 Item['description'] = response.xpath('//div[contains(@class, "page-content")]/div/text()').getall()
@@ -43,4 +47,3 @@ class Tv7Spider(CrawlSpider):
             Item['link_img'] = 'kt.kz'
             yield Item
         else:
-            pass
